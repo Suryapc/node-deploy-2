40e3d53bbdd206090d58537ad91f74a106d8b915
pcsurya <pcsurya21@gmail.com>
2023-05-25 12:58:32 +0530

    deletedfew

--- 25_CallBackFunction.js
+++
@@ -1,9 +0,0 @@
-const fileSystem = require('fs');
-
-// const data = fileSystem.readFileSync('sample.txt');
-// console.log(data.toString());
-
-fileSystem.readFile('sample.txt', function(err, buffer) {
-    if(err) throw (err);
-    console.log(buffer.toString());
-});
